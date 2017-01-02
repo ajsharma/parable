b2d3a1d615b634c8a6799d08703c1cfc9774b234
Ajay Sharma <aj@ajsharma.com>
2017-01-02 02:23:58 +0000

    Adds Pair class to generate list of combinations for participants

--- spec/models/scheduler_spec.rb
+++ spec/models/scheduler_spec.rb
@@ -1,58 +1,52 @@
 # frozen_string_literal: true
+require "./app/models/pair"
 require "./app/models/scheduler"
 
 RSpec.describe Scheduler, ".initialize" do
   let(:mo) { "Mo" }
   let(:mary) { "Mary" }
+  let(:pairs) { Pair.build_combinations(mo, mary) }
 
   it do
     expect { described_class.new([]) }.to raise_error(
       ArgumentError,
-      "Must provide at least one participant"
+      "Must provide at least one pairing"
     )
   end
 
   it do
-    expect { described_class.new([mo, mary]) }.not_to raise_error
+    expect { described_class.new(pairs) }.not_to raise_error
   end
 end
 
 RSpec.describe Scheduler, "#schedule" do
   let(:mo) { "Mo" }
+  let(:mal) { "Mal" }
   let(:mary) { "Mary" }
   let(:moffett) { "Moffett" }
 
-  context "with 1 participants" do
-    let(:subject) do
-      described_class.new([mo]).schedule
-    end
-
-    it do
-      is_expected.to match_array([[[mo, nil]]])
-    end
-  end
-
-  context "with 2 participants" do
-    let(:subject) do
-      described_class.new([mo, mary]).schedule
-    end
-
-    it do
-      is_expected.to match_array([[[mo, mary]]])
-    end
-  end
-
-  context "with 3 participants" do
-    let(:subject) do
-      described_class.new([mo, mary, moffett]).schedule
-    end
+  context "with 1 pair" do
+    let(:pairs) { Pair.build_combinations(mo, mary) }
+    let(:subject) { described_class.new(pairs).schedule }
 
     it do
       is_expected.to match_array(
         [
-          [[mary, nil], [moffett, mo]],
-          [[mo, nil], [mary, moffett]],
-          [[moffett, nil], [mo, mary]]
+          Pair.new(mo, mary)
+        ]
+      )
+    end
+  end
+
+  context "with 4 pairs" do
+    let(:pairs) { Pair.build_combinations(mo, mal, mary, moffett) }
+    let(:subject) { described_class.new(pairs).schedule }
+
+    it do
+      is_expected.to match_array(
+        [
+          Pair.new(mo, mal),
+          Pair.new(mary, moffett)
         ]
       )
     end
